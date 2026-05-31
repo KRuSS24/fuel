01d27cba09c1a08754ea4bb226e6a5d8c929e349
KRuSS24 <russ0273@gmail.com>
2026-05-31 23:45:37 +0300

    commit 2

---
+++ src/main/java/com/home/fuel/DTO/CardLogBook.java
@@ -0,0 +1,11 @@
+package com.home.fuel.DTO;
+
+import java.time.LocalDate;
+
+public class CardLogBook {
+    private FuelCard fuelCard;
+    private FuelStation station;
+    private Driver driver;
+    private LocalDate issueDate;
+
+}
--- src/main/java/com/home/fuel/controllers/mainController.java
+++ src/main/java/com/home/fuel/controllers/mainController.java
@@ -1,13 +1,16 @@
 package com.home.fuel.controllers;
 
+import com.home.fuel.DTO.CardLogBook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class mainController {
 
-    @GetMapping
-    public void getMapp(){
+//    @GetMapping
+    @PostMapping
+    public void post(CardLogBook logBook){
 
     }
 }
