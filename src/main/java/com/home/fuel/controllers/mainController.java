5ad63e11a2a3f5f15c66d7c0591054751dd2a1df
KRuSS24 <russ0273@gmail.com>
2026-06-04 15:59:24 +0300

    Commit from work 04-06

--- src/main/java/com/home/fuel/controllers/mainController.java
+++ src/main/java/com/home/fuel/controllers/mainController.java
@@ -1,7 +1,7 @@
 package com.home.fuel.controllers;
 
 
-import com.home.fuel.entities.FuelCardEntity;
+
 import com.home.fuel.repositories.FuelCardEntityRepo;
 import com.home.fuel.services.DtoService;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/")
 public class mainController {
     private final DtoService dtoService;
 
-    private final FuelCardEntityRepo fuelCardEntityRepo;
     @GetMapping("/fuel_cards")
     public String getCards(Model model){
         model.addAttribute("fuel_cards",dtoService.getFuelCards());
