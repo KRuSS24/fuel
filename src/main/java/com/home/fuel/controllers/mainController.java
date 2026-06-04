fb2fab7095a3d470722c83ea2ca26cf92327fbd1
KRuSS24 <russ0273@gmail.com>
2026-06-04 12:53:42 +0300

    Commit from work 04-06

--- src/main/java/com/home/fuel/controllers/mainController.java
+++ src/main/java/com/home/fuel/controllers/mainController.java
@@ -1,18 +1,14 @@
 package com.home.fuel.controllers;
 
-import com.home.fuel.DTO.CardLogBook;
+
 import com.home.fuel.entities.FuelCardEntity;
 import com.home.fuel.repositories.FuelCardEntityRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,11 +18,7 @@ public class mainController {
     private final FuelCardEntityRepo fuelCardEntityRepo;
     @GetMapping()
     @RequestMapping("/fuel_card")
-//    @ResponseBody()
-//    @PostMapping
     public List<FuelCardEntity> getCards(){
-//        List<FuelCardEntity> fuelcCardList = fuelCardEntityRepo.findAll();// new ArrayList<>();
-//        List<FuelCardEntity> optFuelCardList = fuelCardEntityRepo.findAll();
 
         return  fuelCardEntityRepo.findAll();//fuelcCardList;
     }
